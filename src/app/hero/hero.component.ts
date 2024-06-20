f1f7ea2ca5e25a2a11dd152118a0d44040f83a7e
Anushka6060 <kushananushka12345@gmail.comm>
2024-06-20 11:36:59 +0530

    add video for hero

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -5,6 +5,7 @@ import { createRequestOption } from '../shared/util/request-util';
 import { SERVER_API_URL } from '../shared/util/common-util';
 import { CommonModule, JsonPipe, NgFor, NgIf, isPlatformBrowser } from '@angular/common';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { DomSanitizer, SafeHtml, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-hero',
@@ -15,6 +16,8 @@ import { SlickCarouselModule } from 'ngx-slick-carousel';
 })
 export class HeroComponent implements OnInit {
   bannerList: any = []
+  videoUrl!: string;
+  safeVideoUrl: SafeResourceUrl | null = null;
   slideConfig = {
     slidesToShow: 1,
     nav: true,
@@ -47,15 +50,35 @@ export class HeroComponent implements OnInit {
     },
 
   }
-  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object, private renderer: Renderer2, private elementRef: ElementRef) { }
+  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object, private renderer: Renderer2, private sanitizer: DomSanitizer) {
+
+  }
   ngOnInit(): void {
 
     this.query().subscribe((res: HttpResponse<any>) => {
       this.bannerList = res.body
+      this.videoUrl = res.body[0]?.videoPath
+      this.validateAndSanitizeUrl(this.videoUrl);
     })
   }
   query(req?: any): Observable<any> {
     const options = createRequestOption(req);
     return this.http.get<any[]>(SERVER_API_URL + '/api/banner', { params: options, observe: 'response' });
+  }
+  validateAndSanitizeUrl(url: string): void {
+    if (this.isValidUrl(url)) {
+      this.safeVideoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+      console.log(this.safeVideoUrl);
+    } else {
+      console.error('Invalid URL:', url);
+    }
+  }
+  isValidUrl(url: string): boolean {
+    try {
+      new URL(url);
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
